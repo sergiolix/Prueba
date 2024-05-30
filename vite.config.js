61ceac1f56181d629a0249b17845d24d26d3f8ee
sergiolix <sergiolix8@gmail.com>
2024-05-30 17:29:13 -0500

    agregando

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  base:'https://sergiolix.github.io/Prueba',
   build: {
     rollupOptions: {
       external: ['bootstrap/dist/css/bootstrap.min.css']
