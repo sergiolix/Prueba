09bde566334561ab4136b35b3d145ff877722972
sergiolix <sergiolix8@gmail.com>
2024-05-30 17:20:29 -0500

    boostrap

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {},
+    // Agrega la importación de Bootstrap aquí
+    postcss: {
+      plugins: [
+        require('autoprefixer'),
+        // Importa los estilos de Bootstrap
+        require('postcss-import'),
+        require('tailwindcss'), // Si estás usando TailwindCSS
+        require('postcss-flexbugs-fixes'),
+        // Aquí importa Bootstrap CSS
+        require('bootstrap/dist/css/bootstrap.min.css').default,
+      ],
+    },
+  },
 })
